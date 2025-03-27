18717727e9f198b7f55e32e119b33471fdfac0ce
Agustin Rodriguez <agrodriguez@fi.uba.ar>
2025-03-27 11:39:45 -0300

    trying sh script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 addr="server:12345"
 sent="hola"
-received=$(echo "$sent" | nc "$addr" -w 3)
+received=$(echo "$sent" | nc "$addr")
 if [ "$received" = "$sent" ]; then
 	echo "action: test_echo_server | result: success"
 else
