f38bfb6c8eab1807157a8733d10a36dcd89f79d5
Agustin Rodriguez <agrodriguez@fi.uba.ar>
2025-03-27 02:10:42 -0300

    changed bash script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ip=server
-port=12345
+ip="server"
+port="12345"
 sent="hola"
-received=$(echo $sent | nc $ip $port -w 3)
-if [[ $received == $sent ]]; then
+received=$(echo "$sent" | nc "$ip" "$port" -w 3)
+if [ "$received" = "$sent" ]; then
 	echo "action: test_echo_server | result: success"
 else
 	echo "action: test_echo_server | result: fail"
